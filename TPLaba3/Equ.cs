c2d3e9518ac537297d01f24157410ecd1df5b62e
Dmitrii <99197409+DiaZone@users.noreply.github.com>
2024-08-13 17:28:47 +0300

    Add files via upload

---
+++ TPLaba3/Equ.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPLaba3
+{
+    internal class Equ
+    {
+        public double F1(double x)
+        {
+            if (x<0)
+            {
+                throw new NotFiniteNumberException();
+            }
+            return Math.Pow(Math.Pow(x, 3 / 2) - 2, 1 / 3);
+        }
+
+        public double F2(double x)
+        {
+            return Math.Cos(-Math.Pow(x, 2));
+        }
+
+        public double F3(double x)
+        {
+            if (x == 0)
+            {
+                throw new NotFiniteNumberException();
+            }
+            return Math.Cosh(Math.Pow(2/x, 1/3));
+        }
+
+        public double F4(double x)
+        {
+            double sumF4 = 0;
+            for(int i = 1; i<=1000000; i++) {
+               sumF4 += (i * x * x - 1) * Math.Sqrt(x);
+            }
+            return sumF4;
+        }
+    }
+}
---
+++ TPLaba3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+
+namespace TPLaba3
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Equ Resh = new Equ();
+            double x0, x, h, k;
+            Console.WriteLine("Введите x0: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите кол-во точек: ");
+            k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите шаг: ");
+            h = Convert.ToDouble(Console.ReadLine());
+            double xk = x + (h * k);
+            double F;
+            int count = 0;
+            while (x <= xk)
+            {
+                try
+                {
+                    F = Resh.F1(x) + Resh.F2(x) + Resh.F3(x) + Resh.F4(x);
+                    x += h;
+                    Console.WriteLine(F);
+                }
+                catch (NotFiniteNumberException)
+                {
+                    Console.WriteLine("Error");
+                    x += h;
+                    count++;
+                }
+                if(count == 1)
+                {
+                    System.Windows.MessageBox.Show("Ошибка в расчетах","Ошибка");
+                    count++;
+                }
+            }
+            Console.WriteLine("{0} - количество ошибок ", --count);
+        }
+    }
+}
